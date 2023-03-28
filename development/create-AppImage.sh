ab8dfc82263e81f78dc15287b995021e25461971
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-03-28 23:00:54 +0300

    Run linuxdeploy before linuxdeploy-plugin-qt

--- development/create-AppImage.sh
+++ development/create-AppImage.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 PROG=nixnote2
+DESKTOP_FILE=appdir/usr/share/applications/${PROG}.desktop
 
 BUILD_DIR=$(cat _build_dir_.txt)
 echo Build dir: ${BUILD_DIR}
+
+if [ ! -f "$DESKTOP_FILE" ]; then
+    echo "$DESKTOP_FILE not found!"
+    exit 1
+fi
 
 export VERSIONX=$(cat appdir/usr/share/nixnote2/version.txt)
 echo Version: ${VERSIONX}
 
 export DEBUG=1
+
+CMD="linuxdeploy --desktop-file=$DESKTOP_FILE"
+echo About to run: $CMD
+$CMD
+
 CMD="linuxdeployqt --appdir appdir"
 echo About to run: $CMD
 $CMD
