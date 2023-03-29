7060113af5fc4d94cb7bee6e11be29cc3371d029
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-03-29 21:08:05 +0300

    A couple of fixes + some more debug output

--- development/replace-webkit-ubuntu.sh
+++ development/replace-webkit-ubuntu.sh
@@ -10,6 +10,9 @@ if [ -z "${CUSTOM_WEBKIT_BINARIES_FOLDER}" ]; then
     exit 1
 fi
 
+echo "Installing tree"
+apt-get install -y tree
+
 echo "Removing the files from official QtWebKit packages"
 rm -rf /usr/include/x86_64-linux-gnu/qt5/QtWebKit
 rm -rf /usr/include/x86_64-linux-gnu/qt5/QtWebKitWidgets
@@ -26,33 +29,25 @@ rm -rf /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebPluginProcess
 rm -rf /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebProcess
 rm -rf /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebStorageProcess
 
+echo "Custom QtWebKit files"
+tree ${CUSTOM_WEBKIT_BINARIES_FOLDER}
+
 echo "Copying custom QtWebKit headers"
 mkdir -p /usr/include/x86_64-linux-gnu/qt5
 cp -r ${CUSTOM_WEBKIT_BINARIES_FOLDER}/include/* /usr/include/x86_64-linux-gnu/qt5/
 
 echo "Copying custom QtWebKit libs"
+mkdir -p /usr/lib/x86_64-linux-gnu/
 cp -r ${CUSTOM_WEBKIT_BINARIES_FOLDER}/lib/* /usr/lib/x86_64-linux-gnu/
 
 echo "Copying custom QtWebKit libexec files"
-mkdir -p /usr/include/x86_64-linux-gnu/qt5/libexec
+mkdir -p /usr/lib/x86_64-linux-gnu/qt5/libexec
 cp -r ${CUSTOM_WEBKIT_BINARIES_FOLDER}/libexec/* /usr/lib/x86_64-linux-gnu/qt5/libexec/
 
 echo "Copying custom QtWebKit mkspecs files"
-mkdir -p /usr/include/x86_64-linux-gnu/qt5/mkspecs
+mkdir -p /usr/lib/x86_64-linux-gnu/qt5/mkspecs
 cp -r ${CUSTOM_WEBKIT_BINARIES_FOLDER}/mkspecs/* /usr/lib/x86_64-linux-gnu/qt5/mkspecs/
 
 echo "Copying done. New QtWebKit files:"
-ls -la /usr/include/x86_64-linux-gnu/qt5/QtWebKit
-ls -la /usr/include/x86_64-linux-gnu/qt5/QtWebKitWidgets
-ls -la /usr/lib/x86_64-linux-gnu/cmake/Qt5WebKit
-ls -la /usr/lib/x86_64-linux-gnu/cmake/Qt5WebKitWidgets
-ls -la /usr/lib/x86_64-linux-gnu/pkgconfig/Qt5WebKit.pc
-ls -la /usr/lib/x86_64-linux-gnu/pkgconfig/Qt5WebKitWidgets.pc
-ls -la /usr/lib/x86_64-linux-gnu/qt5/mkspecs/modules/qt_lib_webkit.pri
-ls -la /usr/lib/x86_64-linux-gnu/qt5/mkspecs/modules/qt_lib_webkitwidgets.pri
-ls -la /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebNetworkProcess
-ls -la /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebPluginProcess
-ls -la /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebProcess
-ls -la /usr/lib/x86_64-linux-gnu/qt5/libexec/QtWebStorageProcess
-rm -rf /usr/lib/x86_64-linux-gnu/libQt5WebKit.so*
-rm -rf /usr/lib/x86_64-linux-gnu/libQt5WebKitWidgets.so*
+tree /usr/include/x86_64-linux-gnu/qt5
+tree /usr/lib/x86_64-linux-gnu
