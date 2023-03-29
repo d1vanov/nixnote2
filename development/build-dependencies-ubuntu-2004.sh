cd4c7e2aad1f6834e077aae43421f3a37e564008
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-03-29 11:57:41 +0300

    Add ccache to the list of packages to install for Ubuntu

--- development/build-dependencies-ubuntu-2004.sh
+++ development/build-dependencies-ubuntu-2004.sh
@@ -7,7 +7,7 @@
 sudo add-apt-repository ppa:nixnote/nixnote2-stable -y
 sudo apt-get update -qq
 
-sudo apt-get -y install git-core qt5-default build-essential \
+sudo apt-get -y install git-core qt5-default build-essential ccache \
                             wget curl make pkg-config \
                             libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev libcurl4-openssl-dev \
                             libpoppler-qt5-dev libqt5webkit5-dev qt5-qmake qttools5-dev-tools libqt5sql5-sqlite \
